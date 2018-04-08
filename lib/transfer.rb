71eb95316e0db89ced0040258c7baebf2b6c15e5
Seth Wax <sethlwax@gmail.com>
2018-04-08 07:57:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,11 @@
 class Transfer
+  attr_accessor :status
   attr_reader :sender, :receiver, :amount
+  
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
+    @status = "pending"
   end
 end
