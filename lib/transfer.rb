3f9f41d933f8621fde46c0789b5b1625c0cf7510
Seth Wax <sethlwax@gmail.com>
2018-04-08 07:57:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,15 @@
 class Transfer
   attr_accessor :status
   attr_reader :sender, :receiver, :amount
-  
+
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
     @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
   end
 end
