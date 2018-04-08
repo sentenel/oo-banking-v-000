229672adf734708ef312997512daed44c4c028cc
Seth Wax <sethlwax@gmail.com>
2018-04-08 08:01:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if status == "pending"
+    if status == "pending" && sender.balance >= amount
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
